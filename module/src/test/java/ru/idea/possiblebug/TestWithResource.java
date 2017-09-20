66d3d2fef47953407ef0131293b1a192ec906c47
Bogodukhov Vladimir <v.bogodukhov@socialquantum.ru>
2017-09-20 17:05:00 +0300

    initial

---
+++ module/src/test/java/ru/idea/possiblebug/TestWithResource.java
@@ -0,0 +1,16 @@
+package ru.idea.possiblebug;
+
+/**
+ * Created 20/09/17 16:06
+ *
+ * @author Vladimir Bogodukhov 
+ **/
+public class TestWithResource {
+
+
+    @Test
+    public void test(){
+
+    }
+
+}
