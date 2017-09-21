552d1b01ed26d8f800b3dc297d7e29c5f5ff8185
Bogodukhov Vladimir <v.bogodukhov@socialquantum.ru>
2017-09-21 14:28:03 +0300

    additions

--- module/src/test/java/ru/idea/possiblebug/TestWithResource.java
+++ module/src/test/java/ru/idea/possiblebug/TestWithResource.java
@@ -1,16 +1,42 @@
 package ru.idea.possiblebug;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
 /**
  * Created 20/09/17 16:06
  *
- * @author Vladimir Bogodukhov 
+ * @author Vladimir Bogodukhov
  **/
 public class TestWithResource {
 
 
     @Test
-    public void test(){
-
+    public void testResources() throws IOException {
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(loadResourceByRelativePath(TestWithResource.class, "res/file.txt")))) {
+            int sum = reader.lines().mapToInt(Integer::valueOf).sum();
+            Assert.assertEquals(55, sum);
+        }
     }
+
+    private static InputStream loadResourceByRelativePath(Class startClass, String relativeResourcePath) {
+        Objects.requireNonNull(startClass);
+        Objects.requireNonNull(relativeResourcePath);
+
+        InputStream resource = startClass.getResourceAsStream(relativeResourcePath);
+        if (resource == null) {
+            throw new IllegalArgumentException("resource with path " + relativeResourcePath +
+                    " is not found. search from class " + startClass);
+        }
+        return resource;
+    }
+
 
 }
